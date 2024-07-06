9f9dd91c2f78a378ab90d0f38ee88362e4b640a7
Michelleivan <Its.michelleivan@gmail.com>
2024-07-06 17:06:31 +0800

    set gradient to radial

--- Gradient/script.js
+++ Gradient/script.js
@@ -1,14 +1,17 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".clr1");
 var color2 = document.querySelector(".clr2");
+var color3 = document.querySelector(".clr3");
 var body = document.getElementById("gradient");
 
 function addColor() {
     body.style.background = 
-        "linear-gradient(to right, "
+        "radial-gradient(circle, "
         + color1.value
         + ", "
         + color2.value
+        + ", "
+        + color3.value
         + ")";
 
     css.textContent = body.style.background + ";" ;
@@ -16,4 +19,5 @@ function addColor() {
 
 color1.addEventListener("input", addColor);
 color2.addEventListener("input", addColor);
+color3.addEventListener("input", addColor);
 
